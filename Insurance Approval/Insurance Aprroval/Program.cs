b93969d9f22a5dad0b866d30b1a7b5136d6b1891
Daniel-Briere <110870893+Daniel-Briere@users.noreply.github.com>
2022-11-24 14:52:37 -0700

    added Insurance approval program

---
+++ Insurance Approval/Insurance Aprroval/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance_Aprroval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool ageNum = number > 15;
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            bool stNum = number2 < 3;
+
+            Console.WriteLine("Qualified?");
+            string result = ageNum & !dui & stNum ? "True" : "False";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
