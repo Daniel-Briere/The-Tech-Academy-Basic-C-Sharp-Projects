a8a1dff716d8da3bcbd32dc97c33d76a7730dd90
Daniel-Briere <110870893+Daniel-Briere@users.noreply.github.com>
2022-11-24 13:50:09 -0700

    added loop program

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input reqiurement
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+            do
+            {
+                switch (number)
+                {
+                    //Checks is user entered 62
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    //Checks is user entered 29
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    //Checks is user entered 55
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    //Checks is user entered 12
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    //Checks is user entered any other number
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            //loops the program until user enters right number
+            while (!isGuessed);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
