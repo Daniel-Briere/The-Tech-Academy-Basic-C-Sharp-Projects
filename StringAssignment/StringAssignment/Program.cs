2e526ac71f4b4c6e5d01d06414c3f4408431f030
Daniel-Briere <110870893+Daniel-Briere@users.noreply.github.com>
2022-11-24 15:42:38 -0700

    Added strings assignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "dan";
+            string age = "26";
+            string sex = "male";
+            name = name.ToUpper();
+            StringBuilder pg = new StringBuilder();
+
+            pg.Append("My name is " + name);
+            pg.Append(Environment.NewLine);
+            pg.Append("I am " + age);
+            pg.Append(Environment.NewLine);
+            pg.Append("I am a " + sex);
+
+            Console.WriteLine(pg);
+            Console.ReadLine();
+        }
+    }
+}
