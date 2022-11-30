d3f0dd3f9eaccd556866c757695a554365132efa
Daniel-Briere <daniel.briere01@gmail.com>
2022-11-30 11:27:44 -0700

    added Calling Methods Assignments

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Operator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Operator
+    {
+        public double GetAdd(int data)
+        {
+            data = data + 4;
+            return GetSubtract(data);
+        }
+
+        public double GetSubtract(double data)
+        {
+            data = data - 3;
+            return GetDivide(data);
+        }
+
+        public double GetDivide(double data)
+        {
+            return data / 3;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+            string line = Console.ReadLine();
+            Data = Int32.Parse(line);
+            var operatorObject = new Operator();
+            var result = operatorObject.GetAdd(Data);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
