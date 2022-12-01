2681a2ab9605f5da7b99c54a6ba918bd4d265a39
Daniel-Briere <daniel.briere01@gmail.com>
2022-11-30 17:52:09 -0700

    added main method assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Operator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Operator
+    {
+        public int GetAdd(int data)
+        {
+            return data + 4;
+        }
+
+        public int GetAdd (decimal data)
+        {
+            int intNum = Convert.ToInt32(data);
+            return intNum - 6;
+        }
+
+        public bool GetAdd(string data, out int stgNum)
+        {
+            if (int.TryParse(data, out int intNum))
+            {
+                stgNum = intNum * 4;
+                return true;
+            }
+            else
+            {
+                stgNum = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+            string line = Console.ReadLine();
+            var operatorObject = new Operator();
+
+            if (int.TryParse(line, out int Num))
+            {
+                Console.WriteLine(operatorObject.GetAdd(Num));
+            }
+            else if (decimal.TryParse(line, out decimal Dec))
+            {
+                Console.WriteLine(operatorObject.GetAdd(Dec));
+            }
+            else if (operatorObject.GetAdd(line, out int stgNum))
+            {
+                Console.WriteLine(stgNum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input!!!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
