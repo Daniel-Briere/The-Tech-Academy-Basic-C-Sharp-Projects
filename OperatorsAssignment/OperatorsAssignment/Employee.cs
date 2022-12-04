2ed30869a1b8ad5b364b69df18dea9d6cda2f100
Daniel-Briere <daniel.briere01@gmail.com>
2022-12-03 22:28:16 -0700

    Added Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)//comparing employee IDs with ==
+        {
+            return employee1.Id == employee2.Id ? true : false;
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)//comparing employee IDs with !=
+        {
+            return employee1.Id != employee2.Id ? true : false;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();//instantiating first employee
+            employee1.Id = 1;
+            employee1.FirstName = "Dan";
+            employee1.LastName = "Briere";
+
+            Employee employee2 = new Employee();//instantiating second employee
+            employee2.Id = 2;
+            employee2.FirstName = "Ryan";
+            employee2.LastName = "Kunz";
+
+            Console.WriteLine((employee1 == employee2).ToString());//comparing employee id with overloaded == operator
+            Console.ReadLine();
+        }
+    }
+}
