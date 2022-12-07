7a922f55f8c291381b993d40234c1c59a0d2fe71
Daniel-Briere <daniel.briere01@gmail.com>
2022-12-07 00:01:32 -0700

    Added struct assignment

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    struct Number
+    {
+        public decimal Amount { get; set; }//set date type to deciaml and property to Amount
+
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();//instantiating Number
+            Console.WriteLine(number.Amount = 5m);//Assigning and printing a value
+            Console.ReadLine();
+        }
+    }
+}
