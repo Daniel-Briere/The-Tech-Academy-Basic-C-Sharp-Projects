6f4610ef32d7f5825abe2a3e0c9eaf09ab5a1d2e
Daniel-Briere <daniel.briere01@gmail.com>
2022-12-12 22:43:54 -0700

    Added Input and Date/Time Assignments

---
+++ Date_TimeAssignment/Date_TimeAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_TimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print current time to console
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+
+            //Request number from user
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+
+            //Convert the input to an integer
+            double parsedInput = double.Parse(input);
+
+            //To do add X to current time, X being the number the user entered
+            DateTime newTime = currentTime.AddHours(parsedInput);
+
+            //To do print the result to the console
+            Console.WriteLine(newTime);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the user for a number
+            Console.WriteLine("Enter a number");
+            string numLog = Console.ReadLine();
+
+            //Logs the number to a new text file
+            File.WriteAllText(@"C:\users\nwh15\Logs\log_2.txt", numLog);
+
+            //Prints the file back to the user
+            Console.WriteLine(File.ReadAllText(@"C:\users\nwh15\Logs\log_2.txt"));
+            Console.ReadLine();
+
+        }
+    }
+}
