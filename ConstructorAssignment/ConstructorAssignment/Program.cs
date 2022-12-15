a6e33d9f264fcac774e7a9e4305e3dc548487e68
Daniel-Briere <daniel.briere01@gmail.com>
2022-12-15 16:08:59 -0700

    Added Constructor assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The Constant Variable
+            const string programName = "Constructor Assignment";
+
+            Console.WriteLine("This is my constant variable for the {0}", programName);
+            Console.ReadLine();
+            //variable using "var"
+            Console.WriteLine("Please enter anything");
+            var anyThing = Console.ReadLine();
+            Console.WriteLine("You wrote" + anyThing);
+            Console.ReadLine();
+        }
+            //Chained constructors
+            class Foo
+        {
+            private int id;
+            private string name;
+
+            public Foo() : this(0, "")
+            {
+            }
+
+            public Foo(int id, string name)
+            {
+                this.id = id;
+                this.name = name;
+            }
+
+            public Foo(int id) : this(id, "")
+            {
+            }
+
+            public Foo(string name) : this(0, name)
+            {
+            }
+        }
+    }
+}
