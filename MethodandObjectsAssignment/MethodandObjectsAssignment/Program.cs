e44eb3aa227ac5ff23cff7b19c4e89558b2fde36
Daniel-Briere <daniel.briere01@gmail.com>
2023-01-05 05:02:14 -0700

    added Method and object assignment

---
+++ MethodandObjectsAssignment/MethodandObjectsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodandObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Create a emplyee object
+            Employee employee = new Employee();
+            //Assigning employee name
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            // call superclass method
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
