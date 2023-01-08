e419fd2062957b10514e11ddae5c1f6a50afe61b
Daniel-Briere <daniel.briere01@gmail.com>
2023-01-07 19:01:57 -0700

    added abstract class assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a emplyee object
+            Employee employee = new Employee();
+            //assigning emplyee name
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            //call sayname() method
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
