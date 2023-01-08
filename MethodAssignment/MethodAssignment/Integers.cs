89133fd137c3997124d571b23aedf8a0cc195d81
Daniel-Briere <daniel.briere01@gmail.com>
2023-01-07 18:50:54 -0700

    added methods assignment

---
+++ MethodAssignment/MethodAssignment/Integers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MethodAssignment
+{
+    class Integers
+    {
+        public static void MathOp(int userInput1, [ Optional ] int userInput2)
+        {
+            
+
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Integers integers = new Integers();
+            //request one or two numbers from user
+            Console.WriteLine("Enter a number.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter another number if you would like.");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            //calling method from class
+            integers.MathOp();
+        }
+    }
+}
