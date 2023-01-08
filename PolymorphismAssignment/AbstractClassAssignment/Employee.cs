5aa513261672360bf561ba4c8e2d7501eb99f6cb
Daniel-Briere <daniel.briere01@gmail.com>
2023-01-07 19:38:27 -0700

    added Polymorphism assignment

---
+++ PolymorphismAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AbstractClassAssignment
+{
+    class Employee : Person, IQuittable
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+        public void Quit(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PolymorphismAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a emplyee object
+            Employee employee = new Employee();
+            //assigning emplyee name
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            //call sayname() method
+            employee.SayName();
+            Console.ReadLine();
+            IQuittable IQuittable = new Employee();
+            IQuittable.Quit();
+            Console.ReadLine();
+        }
+    }
+}
